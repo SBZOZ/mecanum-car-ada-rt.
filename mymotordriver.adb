733e986a8e524b555be6da310bcfcdc450708248
SBZOZ <165841302+SBZOZ@users.noreply.github.com>
2025-11-14 17:05:22 +0100

    Update mymotordriver.adb

--- mymotordriver.adb
+++ mymotordriver.adb
@@ -1,10 +1,10 @@
-with HAL;                  use HAL;
-with MicroBit.IOsForTasking; use MicroBit.IOsForTasking;
-with MicroBit.MotorDriver;   use MicroBit.MotorDriver;
+with HAL;                     use HAL;
+with MicroBit.IOsForTasking;  use MicroBit.IOsForTasking;
+with MicroBit.MotorDriver;    use MicroBit.MotorDriver;
 
 package body MyMotorDriver is
 
-   --defining the speed levels using UInt12 (0 .. 4095)
+   -- defining the speed levels using UInt12 (0 .. 4095)
    Speed_Max    : constant UInt12 := 4095;
    Speed_Fast   : constant UInt12 := 3200;
    Speed_Medium : constant UInt12 := 2600;
@@ -12,7 +12,7 @@ package body MyMotorDriver is
 
    procedure DriveForward is
    begin
-      Drive (Forward, (Speed_Max, Speed_Max, Speed_Max, Speed_Max));
+      Drive (Forward,  (Speed_Max, Speed_Max, Speed_Max, Speed_Max));
    end DriveForward;
 
    procedure DriveBackward is
@@ -22,17 +22,17 @@ package body MyMotorDriver is
 
    procedure TurnLeft is
    begin
-      Drive (Left, (Speed_Max, Speed_Max, Speed_Max, Speed_Max));
+      Drive (Left,     (Speed_Max, Speed_Max, Speed_Max, Speed_Max));
    end TurnLeft;
 
    procedure TurnRight is
    begin
-      Drive (Right, (Speed_Max, Speed_Max, Speed_Max, Speed_Max));
+      Drive (Right,    (Speed_Max, Speed_Max, Speed_Max, Speed_Max));
    end TurnRight;
 
    procedure Stop is
    begin
-      Drive (Stop, (0, 0, 0, 0));
+      Drive (Stop,     (0, 0, 0, 0));
    end Stop;
 
    protected body MotorDriver is
