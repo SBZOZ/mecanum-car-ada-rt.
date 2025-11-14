7e348347b6f45b43bb12213aed89ba205d28315c
SBZOZ <165841302+SBZOZ@users.noreply.github.com>
2025-11-14 15:58:15 +0100

    Refactor config.ads for improved readability and units
    Updated comments for clarity and changed measurement units for thresholds.

--- config.ads
+++ config.ads
@@ -1,20 +1,21 @@
 package Config is
-   --Periods (milliseconds)
-   Sense_Period_ms : constant := 200; --20 ms is too fast for ultrasonic sensors, so I just made it a little longer for testing
-   Think_Period_ms : constant := 100; --changed it from 50 to 100 ms to give more time for processing
-   Act_Period_ms   : constant := 40;  --this is stable actuation, but we can possibly reduce it to 20–30 ms after testing
+   --Periods (in milliseconds)
+   Sense_Period_ms : constant := 200;
+   Think_Period_ms : constant := 100;
+   Act_Period_ms   : constant := 40;
 
-   --Task priorities (the higher number => the higher priority)
-   Priority_Think : constant := 3;  --priority
-   Priority_Sense : constant := 2;  --
-   Priority_Act   : constant := 1;  --
+   --priorities(tasks)
+   Priority_Think : constant := 3;
+   Priority_Sense : constant := 2;
+   Priority_Act   : constant := 1;
 
-   --Decision thresholds (in meters, of course)
-   Obstacle_Threshold_m : constant := 0.25;
-   Caution_Threshold_m  : constant := 0.40;
+   --decision thresholds (in centimeters)
+   Obstacle_Threshold_cm : constant := 25;
+   Caution_Threshold_cm  : constant := 40;
 
-   --The motor speed, from the analog_value: value from 0-1023)
-   Speed_Fast   : constant := 512;
-   Speed_Medium : constant := 400;
-   Speed_Slow   : constant := 300;
+   --Motor speeds (0..4095)
+   Speed_Max    : constant := 4095;
+   Speed_Fast   : constant := 3200;
+   Speed_Medium : constant := 2600;
+   Speed_Slow   : constant := 2000;
 end Config;
