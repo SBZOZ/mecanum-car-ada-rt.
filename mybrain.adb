d65a428d43e055fb3bc86542872fd1aba55cf9cd
SBZOZ <165841302+SBZOZ@users.noreply.github.com>
2025-11-14 15:59:01 +0100

    Refactor SensorData procedures in MyBrain package

--- mybrain.adb
+++ mybrain.adb
@@ -1,24 +1,23 @@
+with Ada.Real_Time;
+
 package body MyBrain is
    protected body SensorData is
       procedure SetMeasurements (Left, Right : Distance_CM) is
       begin
-         Left_Dist  := Left;
-         Right_Dist := Right;
+         Left_Dist   := Left;
+         Right_Dist  := Right;
          Update_Time := Ada.Real_Time.Clock;
       end SetMeasurements;
-      
-      function Last_Update return Ada.Real_Time.Time is
-      begin
-         return Update_Time;
-      end Last_Update;
 
       procedure GetMeasurements (Left, Right : out Distance_CM) is
       begin
          Left  := Left_Dist;
          Right := Right_Dist;
       end GetMeasurements;
+
+      function Last_Update return Ada.Real_Time.Time is
+      begin
+         return Update_Time;
+      end Last_Update;
    end SensorData;
-   --SensorData_Instance : SensorData; --no need to define it twice, just did so in the ads
 end MyBrain;
-
-
