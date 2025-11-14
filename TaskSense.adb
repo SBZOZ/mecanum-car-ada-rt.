de959f9b75979f0453c1c311f51f3d7c66fda643
SBZOZ <165841302+SBZOZ@users.noreply.github.com>
2025-11-14 15:56:56 +0100

    Refactor TaskSense to improve sensor handling
    Updated sensor reading logic and periodicity delay.

--- TaskSense.adb
+++ TaskSense.adb
@@ -1,37 +1,31 @@
-with Ada.Real_Time; use Ada.Real_Time;
-with MicroBit;              use MicroBit;
+with Ada.Real_Time;    use Ada.Real_Time;
+with MicroBit;         use MicroBit;
 with MicroBit.Ultrasonic;
-with MyBrain;
 with MicroBit.Console; use MicroBit.Console;
+with MyBrain;
+with Config;
 
 package body TaskSense is
-
    task body Sense is
       myClock : Time;
 
-      --instantiate ultrasonic sensors for both sensors 
-      package Sensor_Left  is new MicroBit.Ultrasonic (MB_P13, MB_P1);  --trig P13, echo P1, translated to actual MicroBit Pins
-      package Sensor_Right is new MicroBit.Ultrasonic (MB_P12, MB_P0);  --trig P12, echo P0
+      package Sensor_Left  is new MicroBit.Ultrasonic (MB_P13, MB_P1);
+      package Sensor_Right is new MicroBit.Ultrasonic (MB_P12, MB_P0);
 
       Dist_Left, Dist_Right : MyBrain.Distance_CM;
    begin
       loop
          myClock := Clock;
 
-         --where the sensors are read
          Dist_Left  := MyBrain.Distance_CM (Sensor_Left.Read);
          Dist_Right := MyBrain.Distance_CM (Sensor_Right.Read);
-         --tried to debug it, to print out to see what the error is
-         Put_Line ("Raw Left=" & Distance_CM'Image(Dist_Left) &
-          " Raw Right=" & Distance_CM'Image(Dist_Right));
 
-         --storing the values in a protected object instance so that other tasks can also read them
+         Put_Line ("Sense: L=" & Distance_CM'Image (Dist_Left) &
+                   " R=" & Distance_CM'Image (Dist_Right));
+
          MyBrain.SensorData_Instance.SetMeasurements (Dist_Left, Dist_Right);
-         --MyBrain.SensorData.SetMeasurements (Dist_Left, Dist_Right);
 
-         --I changed this from 20 to 200, to enforce periodicity
-         delay until myClock + Milliseconds (200); --(200 ms period)
+         delay until myClock + Milliseconds (Config.Sense_Period_ms);
       end loop;
    end Sense;
-
 end TaskSense;
