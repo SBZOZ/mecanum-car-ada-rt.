64beb245a9822db7ffc4ebd1e7bd172900f98f0b
SBZOZ <165841302+SBZOZ@users.noreply.github.com>
2025-11-14 01:24:02 +0100

    Update TaskThink.ads

--- TaskThink.ads
+++ TaskThink.ads
@@ -2,5 +2,6 @@ with MyMotorDriver; use MyMotorDriver;
 with MyBrain;       use MyBrain;
 
 package TaskThink is
-   task Think with Priority => 2; --medium priority
+   task Think with Priority => 3; --medium priority
 end TaskThink;
+
